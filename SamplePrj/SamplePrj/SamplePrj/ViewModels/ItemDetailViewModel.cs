c4de5214102361d0afbf13e25e4b77fa6a102c5f
tklag <49331184+tklag@users.noreply.github.com>
2020-10-10 21:52:05 +0900

    First All Build

---
+++ SamplePrj/SamplePrj/SamplePrj/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SamplePrj.Models;
+
+namespace SamplePrj.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
